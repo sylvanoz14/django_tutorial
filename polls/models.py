57e8bac8764f25b6924e1b4308ad9473fdf1e7e7
Sylvain Momo <momo.syl14@gmail.com>
2016-05-02 23:37:06 +1000

    added tests for the question method

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,9 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def was_pub_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
     def __str__(self):
         return self.question_text
